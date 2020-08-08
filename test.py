96dc9d76cd87cce859443d6f50d7191cd76bb8d8
sarath kumar <nirvaanasarath@gmail.com>
2020-08-08 14:00:42 +0530

    updated with dynamic DBname input

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import sqlite3
 
 dbname = input("Enter your DB name ")
 table_name = input("Enetr your table name ")
-connection = sqlite3.connect("dbname")  #connecting to the specified DB
+connection = sqlite3.connect(dbname)  #connecting to the specified DB
 c = connection.cursor() #cursor() obj is used to execute SQLITE3 statement's 
 c.execute('''CREATE TABLE table1(col1 int PRIMARY KEY,col2 char NOT NULL);''') #creates table
 r = c.execute("SELECT name FROM sqlite_master where type = 'table'") #select all names of tables that exist's in the current DB
