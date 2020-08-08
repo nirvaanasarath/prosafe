6cef88e9962131229bc9dc0f9d90843b44d3c88f
sarath kumar <nirvaanasarath@gmail.com>
2020-08-08 23:27:29 +0530

    test not working

---
+++ prosafe.py
@@ -0,0 +1,24 @@
+def store_file():
+    print("file stored")
+
+
+def open_file():
+    print("file opened")
+
+while(1):
+    print("#"*5 + " WELCOME TO PROSAFE " + "#"*5)
+    print("\n")
+    print("Choose your option: ")
+    print("1.STORE FILE")
+    print("2.OPEN FILE")
+    print("3.QUIT PROSAFE")
+    print("\n")
+  
+    i = int(input(">> "))
+  
+    if i == 1:
+        store_file()
+    elif i == 2:
+        open_file()
+    else:
+        break
