26096fba14fe12f926d0e3dd29b35a48cf539a9a
sarath kumar <nirvaanasarath@gmail.com>
2020-08-08 13:40:54 +0530

    updated with explanation

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import sqlite3
 
-dbname = ("Enter your DB name ")
-connection = sqlite3.connect("dbname")
-c = connection.cursor()
-# c.execute('''CREATE TABLE table1(col1 int PRIMARY KEY,col2 char NOT NULL);''')
-r = c.execute("SELECT name FROM sqlite_master where type = 'table'")
-tables = r.fetchall()
+dbname = input("Enter your DB name ")
+table_name = input("Enetr your table name ")
+connection = sqlite3.connect("dbname")  #connecting to the specified DB
+c = connection.cursor() #cursor() obj is used to execute SQLITE3 statement's 
+c.execute('''CREATE TABLE table1(col1 int PRIMARY KEY,col2 char NOT NULL);''') #creates table
+r = c.execute("SELECT name FROM sqlite_master where type = 'table'") #select all names of tables that exist's in the current DB
+tables = r.fetchall() #fetcheall returns all outpun from the above stmnt.
 for i in tables:
-    print(i)
+    print(i) #prints all o/p
