ca2de42d29acc891427a5e6a65f66127b46ca3fd
sarath kumar <nirvaanasarath@gmail.com>
2020-08-09 13:00:19 +0530

    pass added

--- prosafe.py
+++ prosafe.py
@@ -1,3 +1,8 @@
+import base64
+
+
+org_pass = 123456
+
 def store_file():
     print("file stored")
 
@@ -5,19 +10,28 @@ def store_file():
 def open_file():
     print("file opened")
 
-print("#"*5 + " WELCOME TO PROSAFE " + "#"*5)
-print("\n")
-print("Choose your option: ")
-print("1.STORE FILE")
-print("2.OPEN FILE")
-print("3.QUIT PROSAFE")
-print("\n")
-  
-i = int(input(">> "))
-  
-if i == 1:
-    store_file()
-elif i == 2:
-    open_file()
+# pass = input("Enter your PASSWORD >> ")
+print("enter pass")
+p = int(input())
+
+if p == org_pass:
+    t = 1
+    while(t):
+        print("#"*5 + " WELCOME TO PROSAFE " + "#"*5)
+        print("\n")
+        print("Choose your option: ")
+        print("1.STORE FILE")
+        print("2.OPEN FILE")
+        print("3.QUIT PROSAFE")
+        print("\n")
+        
+        i = int(input(">> "))
+        
+        if i == 1:
+            store_file()
+        elif i == 2:
+            open_file()
+        else:
+            break
 else:
     print("wrong")
