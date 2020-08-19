9a2e7afb173d54a52f3b2a2acf160e07f0e0b0fb
sarath kumar <nirvaanasarath@gmail.com>
2020-08-19 20:53:57 +0530

    git issues solved and project break

--- files.py
+++ files.py
@@ -1,52 +1,51 @@
-import sqlite3
+def user_validation():
+    import sqlite3
 
-con = sqlite3.connect("userdatabase.db")
-c = con.cursor()
-try:
-    c.execute('''CREATE TABLE USERINFO([generated_id] INTEGER PRIMARY KEY,NAME CHAR(20) NOT NULL,PASSWORD CHAR(20) NOT NULL)''')
-except sqlite3.Error as error:
-    print("Table already exist's")
-    
-
-def create_new():
-    new_name = input("Enter your user name")
-    new_psswd = input("Enter your passwd")
-    c.execute('''INSERT INTO USERINFO(NAME,PASSWORD) VALUES(?,?)''',(new_name,new_psswd))
-    con.commit()
-    c.close()
-
-
-ch = int(input("Are you an existing user \n If yes = 1 If no = 0 > "))
-
-if ch == 1:
-    usr_name = input("Enter your user name ")
-    psswd = str(input("Enter your password "))
     con = sqlite3.connect("userdatabase.db")
     c = con.cursor()
-    k = c.execute('''SELECT PASSWORD FROM USERINFO WHERE NAME=?''',(usr_name,))
-    tupledata = k.fetchone() 
-    c.close()
     try:
-        for i in tupledata:
-            key = i
-    except:
-        print("No such user exist's")
-        print("PROSAFE TERMINATED")
-        break
+        c.execute('''CREATE TABLE USERINFO([generated_id] INTEGER PRIMARY KEY,NAME CHAR(20) NOT NULL,PASSWORD CHAR(20) NOT NULL)''')
+    except sqlite3.Error as error:
+        print("Table already exist's")
         
-        
-    # key = tupledata(0,)
 
-    if key == psswd:
-        print("LOGIN SUCESS")
-    else:
-        print("LOGIN FAILED")
+    def create_new():
+        new_name = input("Enter your user name")
+        new_psswd = input("Enter your passwd")
+        c.execute('''INSERT INTO USERINFO(NAME,PASSWORD) VALUES(?,?)''',(new_name,new_psswd))
+        con.commit()
+        c.close()
 
-else:
-    new = int(input("Would you like to create a new PROSAFE account \n IF YES - 1 IF NO (QUIT) - 2 "))
-    if new == 1:
-        create_new()
+
+    ch = int(input("Are you an existing user \n If yes = 1 If no = 0 > "))
+
+    if ch == 1:
+        usr_name = input("Enter your user name ")
+        psswd = str(input("Enter your password "))
+        con = sqlite3.connect("userdatabase.db")
+        c = con.cursor()
+        k = c.execute('''SELECT PASSWORD FROM USERINFO WHERE NAME=?''',(usr_name,))
+        tupledata = k.fetchone() 
+        c.close()
+        try:
+            for i in tupledata:
+                key = i
+        except:
+            print("No such user exist's")
+            print("PROSAFE TERMINATED")
+            exit()
+
+        if key == psswd:
+            print("LOGIN SUCESS")
+        else:
+            print("LOGIN FAILED")
+            exit()
+
     else:
-        print("quit")
-        
+        new = int(input("Would you like to create a new PROSAFE account \n IF YES - 1 IF NO (QUIT) - 2 "))
+        if new == 1:
+            create_new()
+        else:
+            print("quit")
+            exit()
 
--- prosafe.py
+++ prosafe.py
@@ -1,39 +1,26 @@
-import base64
+import files
 
 
-org_pass = 123456
-
+files.user_validation()
 def store_file():
-    type_file = input("What is the extension of the file you want to store today")
+    print("files stored")
     
-
-
-
 def open_file():
     print("file opened")
-
-# pass = input("Enter your PASSWORD >> ")
-print("enter pass")
-p = int(input())
-
-if p == org_pass:
-    t = 1
-    while(t):
-        print("#"*5 + " WELCOME TO PROSAFE " + "#"*5)
-        print("\n")
-        print("Choose your option: ")
-        print("1.STORE FILE")
-        print("2.OPEN FILE")
-        print("3.QUIT PROSAFE")
-        print("\n")
+t = 1
+while(t):
+    print("#"*5 + " WELCOME TO PROSAFE " + "#"*5)
+    print("\n")
+    print("Choose your option: ")
+    print("1.STORE FILE")
+    print("2.OPEN FILE")
+    print("3.QUIT PROSAFE")
+    print("\n")
+    i = int(input(">> "))
         
-        i = int(input(">> "))
-        
-        if i == 1:
-            store_file()
-        elif i == 2:
-            open_file()
-        else:
-            break
-else:
-    print("wrong")
+    if i == 1:
+        store_file()
+    elif i == 2:
+        open_file()
+    else:
+        break
