8cb17923a2848e031e3708e72d29654416e25479
sarathkumar365 <sarathkumark.s365@gmail.com>
2020-08-25 20:00:14 +0530

    prosafe

--- prosafe.py
+++ prosafe.py
@@ -26,7 +26,7 @@ def open_stored_file(usr_ch,fname,fext):
         c.execute("SELECT DATA FROM USERDATAS WHERE FILENAME=?",(fname,))
         d = c.fetchone()
         for i in d:
-            data = i[0]
+            data = i
         datas = bytes(data,'utf-8')
         with open (fname+'.'+fext,"wb") as f:
             f.write(datas)
@@ -49,7 +49,7 @@ def store_file():
     con = sqlite3.connect("userdatabase.db")
     c = con.cursor()
     try:
-        c.execute('''CREATE TABLE IF NOT TABLE EXIST USERDATAS(FILENAME VARCHAR(20),FILEEXT VARCHAR(5),DATA BLOB)''')
+        c.execute('''CREATE TABLE  USERDATAS(FILENAME VARCHAR(20),FILEEXT VARCHAR(5),DATA BLOB)''')
     except:
         print("table already exists")
     finally:
@@ -67,8 +67,8 @@ def store_file():
             file_data = f.read()
         text_store(file_name,file_ext,file_data)
     elif usr_ch == int(2):
-        fname = input("Enter your file name")
-        fext = input("Enter your file ext")
+        fname = input("Enter your file name  ")
+        fext = input("Enter your file ext  ")
         with open(fname+"."+fext,"rb") as f:
             data = f.read()
         image_store(fname,fext,data)
@@ -92,6 +92,14 @@ def open_file():
         print("YOUR FILE IS SUCESSFULLY RESTORED")
     else:
         print("FILE RESTORATION FAILED")
+
+def show_files():
+    con = sqlite3.connect("userdatabase.db")
+    c = con.cursor()
+    c.execute("SELECT FILENAME FROM USERDATAS")
+    data = c.fetchall()
+    for i in data:
+        print(i[0])
     
 
 t = 1
@@ -101,7 +109,8 @@ while(t):
     print("Choose your option: ")
     print("1.STORE FILE")
     print("2.OPEN FILE")
-    print("3.QUIT PROSAFE")
+    print("3.SHOW FILES")
+    print("4.QUIT PROSAFE")
     print("\n")
     i = int(input(">> "))
         
@@ -109,5 +118,9 @@ while(t):
         store_file()
     elif i == 2:
         open_file()
+    elif i == 3:
+        show_files()
     else:
-        break
+        exit()
+
+        
